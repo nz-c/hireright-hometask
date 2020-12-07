04f6fed777341300f25b4f89a6352cc42ff2e54b
Aleksei Nazarov <aleksei_n@hotmail.com>
2020-12-07 15:24:00 +0200

    Commit unit tests

---
+++ HomeTask/src/test/java/com/hireright/hometask/test/GeoLocationTests.java
@@ -0,0 +1,99 @@
+package com.hireright.hometask.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.hireright.hometask.service.Service;
+import com.hireright.hometask.service.ServiceRegistry;
+import com.hireright.hometask.service.ServiceRequest;
+
+public class GeoLocationTests {
+	
+	@Test
+	public void testOkRequest() {
+		Service service = getService();
+		
+		// create and test request
+		Map<String, String[]> params = null;
+		ServiceRequest request = null;
+		StringWriter writer = null;
+		
+		params = new HashMap<>();
+		params.put("cityCode", new String[] {"TLN"});
+		request = new ServiceRequest("GET", params);
+		writer = new StringWriter();
+				
+		try {
+			assertEquals(Service.STATUS.OK , service.processAndWrite(request, writer));
+			assertEquals("\"Cloudy 4\u00B0C, Percipitation 30%, Wind 5m/s NW\"", writer.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testIncorrectRequest() {
+		Service service = getService();
+		
+		// create and test request
+		Map<String, String[]> params = null;
+		ServiceRequest request = null;
+		StringWriter writer = null;
+		
+		params = new HashMap<>();
+		params.put("cityCodeWrong", new String[] {"TLN"});
+		request = new ServiceRequest("GET", params);
+		writer = new StringWriter();
+		
+		try {
+			assertEquals(Service.STATUS.INVALID_REQUEST , service.processAndWrite(request, writer));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testNotFound() {
+		Service service = getService();
+		
+		// create and test request
+		Map<String, String[]> params = null;
+		ServiceRequest request = null;
+		StringWriter writer = null;
+		
+		params = new HashMap<>();
+		params.put("cityCode", new String[] {"XYZ"});
+		request = new ServiceRequest("GET", params);
+		writer = new StringWriter();
+		
+		try {
+			assertEquals(Service.STATUS.NOTHING , service.processAndWrite(request, writer));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Service getService() {
+		assertNotNull(ServiceRegistry.getInstance());
+		
+		try {
+			ServiceRegistry.getInstance().loadAndRegister(new FileInputStream(new File("target/classes/serviceWeather.json")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// check service is created
+		Service service = ServiceRegistry.getInstance().get("/weather").orElse(null);
+		assertNotNull(service);
+		
+		return service;
+	}
+}
---
+++ HomeTask/src/test/java/com/hireright/hometask/test/WeatherTests.java
@@ -0,0 +1,120 @@
+package com.hireright.hometask.test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.StringWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Test;
+
+import com.hireright.hometask.service.Service;
+import com.hireright.hometask.service.ServiceRegistry;
+import com.hireright.hometask.service.ServiceRequest;
+
+public class WeatherTests {
+	
+	@Test
+	public void testOkRequest() {
+		Service service = getService();
+		
+		// create and test request
+		Map<String, String[]> params = null;
+		ServiceRequest request = null;
+		StringWriter writer = null;
+		
+		params = new HashMap<>();
+		params.put("zipCode", new String[] {"1000"});
+		request = new ServiceRequest("POST", params);
+		writer = new StringWriter();
+				
+		try {
+			assertEquals(Service.STATUS.OK , service.processAndWrite(request, writer));
+			assertEquals("\"Cloudy 4\u00B0C, Percipitation 30%, Wind 5m/s NW\"", writer.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testIncorrectRequest() {
+		Service service = getService();
+		
+		// create and test request
+		Map<String, String[]> params = null;
+		ServiceRequest request = null;
+		StringWriter writer = null;
+		
+		params = new HashMap<>();
+		params.put("zipCodeWrong", new String[] {"1000"});
+		request = new ServiceRequest("POST", params);
+		writer = new StringWriter();
+		
+		try {
+			assertEquals(Service.STATUS.INVALID_REQUEST , service.processAndWrite(request, writer));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testIncorrectMethodRequest() {
+		Service service = getService();
+		
+		// create and test request
+		Map<String, String[]> params = null;
+		ServiceRequest request = null;
+		StringWriter writer = null;
+		
+		params = new HashMap<>();
+		params.put("zipCode", new String[] {"1000"});
+		request = new ServiceRequest("GET", params);
+		writer = new StringWriter();
+		
+		try {
+			assertEquals(Service.STATUS.INVALID_REQUEST , service.processAndWrite(request, writer));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testNotFound() {
+		Service service = getService();
+		
+		// create and test request
+		Map<String, String[]> params = null;
+		ServiceRequest request = null;
+		StringWriter writer = null;
+		
+		params = new HashMap<>();
+		params.put("zipCode", new String[] {"9999"});
+		request = new ServiceRequest("GET", params);
+		writer = new StringWriter();
+		
+		try {
+			assertEquals(Service.STATUS.NOTHING , service.processAndWrite(request, writer));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public Service getService() {
+		assertNotNull(ServiceRegistry.getInstance());
+		
+		try {
+			ServiceRegistry.getInstance().loadAndRegister(new FileInputStream(new File("target/classes/serviceGeoLocation.json")));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		// check service is created
+		Service service = ServiceRegistry.getInstance().get("/location").orElse(null);
+		assertNotNull(service);
+		
+		return service;
+	}
+}
